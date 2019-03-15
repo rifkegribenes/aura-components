7f99ddeb1772d3476206a2da886abf65e6e19cd7
rifkegribenes <rifkegribenes@gmail.com>
2019-03-15 16:38:19 -0700

    refactor camping list app

--- campingListController.js
+++ campingListController.js
@@ -1,47 +1,42 @@
 ({
-    clickCreateItem: function(component, event, helper) {
-        var validItem = component.find('itemform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        // If we pass error checking, do some real work
-        if(validItem){
-            // Create the new item
-            var newItem = component.get("v.newItem");
-            console.log("Create item: " + JSON.stringify(newItem));
-            helper.createItem(component, newItem);
-        }
-    },
-    createItem: function(component, item) {
-        var theItems = component.get("v.items");
- 
-        // Copy the item to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        var newItem = JSON.parse(JSON.stringify(item));
- 	    var defaultItem = { 'sobjectType': 'Camping_Item__c',
-                        'Quantity__c': 0,
-                        'Price__c': 0 };
-        theItems.push(newItem);
-        component.set("v.items", theItems);
-        component.set("v.newItem", defaultItem);
-    },
-     // Load items from Salesforce
-    doInit: function(component, event, helper) {
-        // Create the action
-        var action = component.get("c.getItems");
-        // Add callback behavior for when response is received
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.items", response.getReturnValue());
-            }
-            else {
-                console.log("Failed with state: " + state);
-            }
-        });
-        // Send action off to be executed
-        $A.enqueueAction(action);
-    },
+    // Load items from Salesforce
+	doInit: function(component, event, helper) {
 
+    // Create the action
+    var action = component.get("c.getItems");
+
+    // Add callback behavior for when response is received
+    action.setCallback(this, function(response) {
+        var state = response.getState();
+        if (component.isValid() && state === "SUCCESS") {
+            component.set("v.items", response.getReturnValue());
+        }
+        else {
+            console.log("Failed with state: " + state);
+        }
+    });
+
+    // Send action off to be executed
+    $A.enqueueAction(action);
+},
+
+    
+    handleAddItem: function(component, event, helper) {
+    //   var newItem = event.getParam("item");
+    //helper.addItem(component, newItem);
+    var action = component.get("c.saveItem");
+    		action.setParams({"item": newItem});
+    		action.setCallback(this, function(response){
+        		var state = response.getState();
+        		if (component.isValid() && state === "SUCCESS") {
+            		// all good, nothing to do.
+            var items = component.get("v.items");
+            items.push(response.getReturnValue());
+            component.set("v.items", items);
+        		}
+    		});
+    		$A.enqueueAction(action);
+        		}
+    
+              
 })
--- campingListHelper.js
+++ campingListHelper.js
@@ -1,18 +1,13 @@
 ({
-    createItem: function(component, expense) {
-       var action = component.get("c.saveItem");
-       action.setParams({
-           "item": item
-       });
-       action.setCallback(this, function(response){
-           var state = response.getState();
-           if (state === "SUCCESS") {
-               var items = component.get("v.items");
-               items.push(response.getReturnValue());
-               component.set("v.items", items);
-           }
-       });
-       $A.enqueueAction(action);
-   },
-})
-
+    addItem: function(component, item) {
+     this.saveItem(component, item, function(response){
+         var state = response.getState();
+         if (component.isValid() && state === "SUCCESS") {
+             // all good, nothing to do.
+          /*   var items = component.get("v.items");
+             items.push(response.getReturnValue());
+             component.set("v.items", items);*/
+         }
+     });
+ },
+ })
