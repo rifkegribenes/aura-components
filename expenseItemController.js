24f9b0d53826d9bbdacb038454018ee8cc92777e
rifkegribenes <rifkegribenes@gmail.com>
2019-03-15 16:00:51 -0700

    add event for expenses app, refactor

--- expenseItemController.js
+++ expenseItemController.js
@@ -1,4 +1,10 @@
 ({
+    clickReimbursed: function(component, event, helper) {
+        var expense = component.get("v.expense");
+        var updateEvent = component.getEvent("updateExpense");
+        updateEvent.setParams({ "expense": expense });
+        updateEvent.fire();
+    },
     doInit : function(component, event, helper) {
         var mydate = component.get("v.expense.Date__c");
         if(mydate){
